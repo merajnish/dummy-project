cc41face739378b8275987aecfe67c2b466f324b
merajnish <46785754+merajnish@users.noreply.github.com>
2019-02-01 14:01:00 +0530

    editable

---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+        <title>E-commerce</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        
+        <style>
+            
+            .test1
+            {
+                background-color: red;
+            }
+            .test2
+            {
+                background-color: gray;
+            }
+        </style>
+        
+    </head>
+    <body>
+        
+        <div class="col-md-12" style="background-color: blue;">dgfs</div>
+        <div class="container-fluid" style="background-color: green; padding: 0;">
+            <div class="col-md-3" style="padding: 0;">
+                <div class="list-group">
+                  <a href="#" class="list-group-item active">
+                    <h4 class="list-group-item-heading">CATEGORIES</h4>
+                    <ul class="list-group">
+                      <a href="#"><li class="list-group-item list-group-item-success">First item</li></a>
+                      <li class="list-group-item list-group-item-info">Second item</li>
+                      <li class="list-group-item list-group-item-warning">Third item</li>
+                      <li class="list-group-item list-group-item-danger">Fourth item</li>
+                    </ul>
+                  </a>
+                </div>
+            </div>
+            <div class="col-md-9 test2" style="margin-left: 0;">
+
+            </div>
+        </div>
+        <div class="col-md-12 bg-danger">dsgfdg</div>
+        
+    </body>
+</html>
